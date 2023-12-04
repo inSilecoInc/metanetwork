bfc04bd50322dd697594c77265488b67d498a841
Kevin Cazelles <kevin.cazelles@insileco.io>
2023-12-04 17:31:36 -0500

    Add focus field

--- inst/app/global.R
+++ inst/app/global.R
@@ -7,6 +7,11 @@ nodes <- system.file("extdata", "nodes.csv", package = "metanetwork") |>
 links <- system.file("extdata", "links.csv", package = "metanetwork") |>
     read.csv()
 
+grp_foc <- as.list(unique(nodes$subnetwork))
+names(grp_foc) <- unlist(grp_foc)
+grp_foc <- c(All = "", grp_foc)
+
+
 nodeColPal <- c(
     "viridis",
     "magma",
